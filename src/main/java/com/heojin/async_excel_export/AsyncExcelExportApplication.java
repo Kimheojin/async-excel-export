ebc7b89c81f1a179466b663ade9d1d6ddb604bbc
Jin <hurjin1109@naver.com>
2026-06-03 12:42:25 +0900

    INIT

---
+++ src/main/java/com/heojin/async_excel_export/AsyncExcelExportApplication.java
@@ -0,0 +1,13 @@
+package com.heojin.async_excel_export;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncExcelExportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncExcelExportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heojin/async_excel_export/AsyncExcelExportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heojin.async_excel_export;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncExcelExportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
